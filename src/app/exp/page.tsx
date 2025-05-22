b67cdde4e848b6ceee65ad295570fb60609ad7f1
Ethan <ejtarlov@me.com>
2025-05-22 00:10:43 -0700

    Fixed type error

--- src/app/exp/page.tsx
+++ src/app/exp/page.tsx
@@ -30,6 +30,8 @@
           scrollers.forEach((scroller) => {
             scroller.setAttribute("data-animated", "true");
             const scrollerInner = scroller.querySelector(".scroller__inner");
+            if (!scrollerInner) return;
+            
             const scrollerContent = Array.from(scrollerInner.children);
             scrollerContent.forEach((item) => {
               const duplicatedItem = item.cloneNode(true) as HTMLElement;
