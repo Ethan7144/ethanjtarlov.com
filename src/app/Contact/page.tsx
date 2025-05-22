076c189c4d6332dccdafc0cb6d467c8f56fedfbc
Ethan <ejtarlov@me.com>
2025-05-21 23:21:07 -0700

    Added Pages and Navigation

---
+++ src/app/Contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Contact</h1>
+      <p>This page will show my contact info.</p>
+    </main>
+  );
+}
