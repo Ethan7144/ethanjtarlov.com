add38a6679bdb7cccefde015a33a5d3ee76c8363
Ethan Tarlov <ejtarlov@me.com>
2025-05-21 23:27:19 -0700

    Added Pages and Navigation (#13)
    * Added Pages and Navigation
    
    * Fixed build error

---
+++ src/app/Socials/page.tsx
@@ -0,0 +1,8 @@
+export default function SocialsPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Socials</h1>
+      <p>This page will show my socials..</p>
+    </main>
+  );
+}
