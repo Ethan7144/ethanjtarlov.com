df2437968de026899aae13646f92e094d0d2acd8
Ethan Tarlov <ethan.tarlov@cimoninc.com>
2025-06-02 12:06:59 -0700

    Fixed build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,19 @@ export default function HomePage() {
       </h1>
 
       <p className="text-center text-lg font-mono">
-        Software Engineer | SU Comp Sci 23' | San Diego ⇌ Las Vegas
+        Software Engineer | SU Comp Sci 23&apos; | San Diego ⇌ Las Vegas
       </p>
-      <hr className="my-10 border-[var(--foreground)] opacity-20 w-1/2" />
-      <section className="animate-fade-in duration-700 delay-200 max-w-xl text-center text-md font-sans leading-relaxed">
-          <p className="mb-4">
-            Hello there! My name is Ethan, and I am currently a Software Engineer on the Gateway team at <strong>CIMON Automation</strong>.
-            I work on enabling communication between industrial devices and our HMI application, Canvas.
-          </p>
-          <p>
-            My team implements and maintains protocol support (like Modbus), sends and receives frame data, and ensures it integrates
-            seamlessly into the runtime environment.
-          </p>
-        </section>
+      <hr className="my-10 border-[var(--foreground)] opacity-20 w-1/2"/>
+          <section className="max-w-xl text-center text-md font-sans leading-relaxed">
+            <p className="mb-4">
+              Hello there! My name is Ethan, and I am currently a Software Engineer on the Gateway team at <strong>CIMON Automation</strong>.
+              I work on enabling communication between industrial devices and our HMI application, Canvas.
+            </p>
+            <p>
+              My team implements and maintains protocol support (like Modbus), sends and receives frame data, and ensures it integrates
+              seamlessly into the runtime environment.
+            </p>
+          </section>
     </div>
   )
 }
