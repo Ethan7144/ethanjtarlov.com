4a98ab0a7c05f72b8d823ff94801c3bad5963435
Ethan Tarlov <ethan.tarlov@cimoninc.com>
2025-06-03 13:27:21 -0700

    Changed font to mono

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[var(--background)] text-[var(--foreground)] px-4">
+    <div className="min-h-screen flex flex-col font-mono items-center justify-center bg-[var(--background)] text-[var(--foreground)] px-4">
       <Head>
         <title>Ethan Tarlov</title>
         <meta name="description" content="Software Engineer | Embedded Systems @ CIMON" />
@@ -18,11 +18,11 @@ export default function HomePage() {
       </p>
       <hr className="my-10 border-[var(--foreground)] opacity-20 w-1/2"/>
           <section className="max-w-xl text-center text-md font-sans leading-relaxed">
-            <p className="mb-4">
+            <p className="mb-4 font-mono">
               Hello there! My name is Ethan, and I am currently a Software Engineer on the Gateway team at <strong>CIMON Automation</strong>.
               I work on enabling communication between industrial devices and our HMI application, Canvas.
             </p>
-            <p>
+            <p className="mb-4 font-mono">
               My team implements and maintains protocol support (like Modbus), sends and receives frame data, and ensures it integrates
               seamlessly into the runtime environment.
             </p>
